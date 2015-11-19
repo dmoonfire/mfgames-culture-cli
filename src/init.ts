312993f14623fda59fb74f14dc14636689a113cd
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-11-19 00:16:13 -0600

    Working on the initial commit.

---
+++ src/init.ts
@@ -0,0 +1,60 @@
+/// <reference path="../typings/node/node.d.ts"/>
+/// <reference path="../typings/yargs/yargs.d.ts"/>
+/// <reference path="../node_modules/mfgames-culture-es6/package.d.ts"/>
+
+import * as yargs from "yargs";
+import * as fs from "fs";
+import * as path from "path";
+import * as mfc from "mfgames-culture";
+
+// Set up and parse the command line arguments.
+var convertHelp = "Convert various text formats into another one.";
+
+function getConvertArguments(y: any) {
+    y.help("help");
+    y.demand(3)
+    y.argv;
+}
+// Combine everything together for the final option object.
+var argv = yargs
+    .usage("$0 command")
+    .help("help")
+    .showHelpOnFail(true, "Specify --help for available options")
+    .demand(1)
+    .command("convert", convertHelp, getConvertArguments)
+    .argv;
+
+// Grab the first elements in the argv, that will be the virtual command we are
+// running. Once we have that, pass it into the appropriate function.
+var commandName = argv._.splice(0, 1)[0];
+
+switch (commandName) {
+    case "convert":
+		runConvert(argv._);
+        break;
+
+    default:
+        yargs.showHelp();
+        break;
+}
+
+function verifyDataDirectory(directory: string): boolean {
+	// Make sure the directory exists.
+	var stats = fs.lstatSync(directory);
+
+	if (!stats.isDirectory) {
+		console.log(directory + " is not a directory");
+		return false;
+	}
+
+	// Create a Nodedirectory mapper from this.
+	return false;
+}
+
+function runConvert(args: string[]): void {
+	// The first argument is always the data directory.
+	var dataDirectory = args.splice(0, 1)[0];
+	if (!verifyDataDirectory(dataDirectory)) { return; }
+
+	console.log("convert_run", argv);
+}
