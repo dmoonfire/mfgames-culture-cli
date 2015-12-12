99f4a454c3e69d71558b3c116219209d7f63fe56
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-12-12 01:16:01 -0600

    Added a "pipe" operation for working with duplex IPC.

--- src/init.ts
+++ src/init.ts
@@ -7,6 +7,7 @@
 import * as yargs from "yargs";
 import * as fs from "fs";
 import * as path from "path";
+import * as readline from "readline";
 import * as mfc from "mfgames-culture-js";
 import * as mfcn from "mfgames-culture-node";
 import { Promise } from "es6-promise";
@@ -22,6 +23,17 @@ function getConvertArguments(y: any) {
     y.demand(3);
     y.argv;
 }
+
+var pipeHelp = "Open a pipe that takes JSON input until closed.";
+
+function getPipeArguments(y: any) {
+    y.option("verbose", { alias: "v", type: "boolean" });
+    y.option("data", { alias: "d", type: "string" });
+    y.help("help");
+    y.demand(1);
+    y.argv;
+}
+
 // Combine everything together for the final option object.
 var argv = yargs
     .usage("$0 command")
@@ -29,6 +41,7 @@ var argv = yargs
     .showHelpOnFail(true, "Specify --help for available options")
     .demand(1)
     .command("convert", convertHelp, getConvertArguments)
+    .command("pipe", pipeHelp, getPipeArguments)
     .argv;
 
 // Grab the first elements in the argv, that will be the virtual command we are
@@ -41,6 +54,10 @@ var cultureProvider: mfc.CultureProvider;
 switch (commandName) {
     case "convert":
         runConvert(argv._);
+        break;
+
+    case "pipe":
+        runPipe(argv._);
         break;
 
     default:
@@ -73,46 +90,143 @@ function setDataDirectory(): boolean {
     return true;
 }
 
+interface ConvertArguments {
+    format?: string;
+    input: string;
+    culture: string;
+}
+
+var cultures: { [id: string]: mfc.Culture; } = { };
+var processIndex = 0;
+var processedIndex = 0;
+
+function convert(args: ConvertArguments) {
+    // We want to handle these in sequential order. We use the processIndex and
+    // processedIndex to figure out the order. processedIndex is incremented as
+    // part of the innermost function.
+    var currentIndex = processIndex++;
+
+    // Start going into the covertTurn which ensures order.
+    convertTurn(args, currentIndex);
+}
+
+function convertTurn(args: ConvertArguments, currentIndex: number) {
+    // See if it is our turn to process. If it isn't, set up a timeout to loop
+    // again until we are ready.
+    if (currentIndex > processedIndex) {
+        setTimeout(function(){convertTurn(args, currentIndex)}, 10);
+        return;
+    }
+
+    // Grab the culture and figure out if we have already cached it or not.
+    var cultureId = args.culture;
+
+    if (cultureId in cultures) {
+        // The culture has been already used, so just directly use it.
+        if (verbose) { console.error("using cached culture", cultureId); }
+        convertCulture(cultures[cultureId], args);
+    } else {
+        // The culture hasn't been used, so we have to use a promise to load
+        // it into memory.
+        if (verbose) { console.log("using culture:", cultureId); }
+        var culturePromise = cultureProvider.getCulturePromise(cultureId);
+
+        culturePromise.then(function(c) {
+            if (verbose) { console.error("loaded culture", cultureId); }
+            convertCulture(c, args);
+
+            // Cache the results so we don't have to hit the disk for reuse.
+            cultures[cultureId] = c;
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+}
+
+function convertCulture(c: mfc.Culture, args: ConvertArguments) {
+    // Grab the input from the arguments.
+    var input = args.input;
+    var format = args.format;
+
+    // Get the instant from parsing the input.
+    if (verbose) { console.log("input: " + input + " (" + format + ")"); }
+
+    try {
+        var instant = c.parseInstant(input);
+
+        // Figure out how we are going to format.
+        var lowerFormat = format.toLowerCase();
+
+        if (lowerFormat === "jdn" || lowerFormat === "julian") {
+            console.log(instant.julian);
+        }
+        else if (lowerFormat === "json") {
+            console.log(JSON.stringify(instant, null, 0));
+        } else {
+            var output = c.formatInstant(instant, format);
+            console.log(output);
+        }
+    } catch (exception) {
+        console.log("Cannot parse ", input + ": " + exception);
+        throw new Error(exception);
+    } finally {
+        processedIndex++;
+    }
+}
+
 function runConvert(args: string[]): void {
     // The first argument is always the data directory.
     if (!setDataDirectory()) { return; }
 
-    // Figure out which format we want.
-    var format = argv.format ? argv.format : "jdn";
+    // Pull out the culture from the first.
+    var culture = args.splice(0, 1)[0];
 
-    // The second is the culture ID which we load as a promise.
-    var cultureId = args.splice(0, 1)[0];
-    var culturePromise = cultureProvider.getCulturePromise(cultureId);
+    // Loop through the rest of the arguments and parse them as input.
+    for (var input of args) {
+        // Populate the arguments to request a conversion.
+        var cargs: ConvertArguments = {
+            format: argv.format ? argv.format : "jdn",
+            culture: culture,
+            input: input
+        };
 
-    if (verbose) { console.log("culture:", cultureId); }
+        // Parse the format.
+        convert(cargs);
+    }
+}
 
-    culturePromise.then(function(c) {
-        // We have a culture, so loop through the remaining elements.
-        for (var input of args) {
-            // Get the instant from parsing the input.
-            if (verbose) { console.log("input: " + input + " (" + format + ")"); }
+function runPipe(args: string[]): void {
+    // The first argument is always the data directory.
+    if (!setDataDirectory()) { return; }
 
-            try {
-                var instant = c.parseInstant(input);
+    // Get the process and hook to the standard input.
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+    });
 
-                // Figure out how we are going to format.
-                var lowerFormat = format.toLowerCase();
+    rl.on('line', function (line: string) {
+        // Convert the buffer to a single string.
+        if (!line || line === "") { process.exit(); }
 
-                if (lowerFormat === "jdn" || lowerFormat === "julian") {
-                    console.log(instant.julian);
-                }
-                else if (lowerFormat === "json") {
-                    console.log(instant);
-                } else {
-                    var output = c.formatInstant(instant, format);
-                    console.log(output);
-                }
-            } catch (exception) {
-                console.log("Cannot parse", input + ": " + exception);
-                throw new Error(exception);
-            }
+        // Try to parse the value.
+        var cargs: ConvertArguments;
+
+        try {
+            cargs = JSON.parse(line);
         }
-    }).catch(function(err) {
-        console.log(err);
+        catch (exception) {
+            var message: string = exception.toString();
+            var err: any = { "error": true, "message": message, "input": line };
+            console.log(JSON.stringify(err, null, 0))
+            return;
+        }
+
+        // Normalize the format.
+        if (!cargs.format) { cargs.format = "jdn"; }
+
+        // Convert the output and output the results.
+        convert(cargs);
     });
 }
